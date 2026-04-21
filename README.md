85a870cc1888421ef252dc1211d1bf7b3bc50382
Zephyr Du <elecleus@outlook.com>
2026-04-21 16:13:06 +0800

    chore: add license section and SPDX identifiers

--- README.md
+++ README.md
@@ -2,6 +2,10 @@
 
 A chess game.
 
+### License
+
+This project is Licensed under **GPL-3.0-or-later**.
+
 ### Thanks to
 
 - **raylib-zig**
--- src/main.zig
+++ src/main.zig
@@ -1,3 +1,7 @@
+// SPDX-FileContributor: Zephyr Du <elecleus@outlook.com>
+//
+// SPDX-License-Identifier: GPL-3.0-or-later
+
 const rl = @import("raylib");
 
 pub fn main() !void {}
